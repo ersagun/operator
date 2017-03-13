a7973d63f190ee7a89dc8778859f78c10e9beeba
ersagun <ersagunyalcintepe@gmail.com>
2017-03-13 14:17:41 +0100

    changed

--- src/main/java/org/miage/m2sid/openlp/openlp/web/OperatorController.java
+++ src/main/java/org/miage/m2sid/openlp/openlp/web/OperatorController.java
@@ -9,48 +9,31 @@ import org.springframework.web.bind.annotation.*;
 
 import reactor.core.publisher.EmitterProcessor;
 import reactor.core.publisher.Mono;
+
 @RestController
 @EnableAutoConfiguration
 public class OperatorController {
 
-    @RequestMapping("/getInformation/{text}")
-    public @ResponseBody  Mono<String> home(@PathVariable(value="text") String text) {
-Generator g= new Generator(text);
-		
-		String[] val= g.getSample().toString().split(" ");
-		for(String s : val){
-			String[] s2=s.split("_");
-			if(s2[1].equals("VB"))
-				System.out.println(s);
-			
-		}
-		
-		
-		// A Flux is a data publisher
-				EmitterProcessor<String> stream = EmitterProcessor.<String>create().connect();
+	/**
+	 * Request from developer is coming, it's text then I take text and thanks
+	 * to Open lp natural language library split the request text of user for
+	 * example : deploy app2 in e2e
+	 * 
+	 * 
+	 * I want to know if there is a mecanisme already implemented in
+	 * reactor.core and webflux for getting ALL flux Example : Developer A
+	 * Called API for deploying app2 Developer B started deploying app3 etc..
+	 * What I would like to do is, having a get for example /activities that my
+	 * bot call, and each time /activities called i want to get all current
+	 * activities (activities started and doesn't finished (a list of flux so))
+	 * 
+	 * @param text
+	 * @return
+	 */
+	@RequestMapping("/getInformation/{text}")
+	public @ResponseBody Mono<String> home(@PathVariable(value = "text") String text) {
+		Generator g = new Generator(text);
+		return Mono.just(g.getSample().toString());
 
-				// Filter values passing through the Flux, observe and capture the result once.
-				Mono<List<String>> promise = stream.filter(s -> s.startsWith("Hello"))
-				                                   .doOnNext(s -> System.out.println("yollo"+1+2+""+s))
-				                                   .collectList()
-				                                   .subscribe();
-
-				// Publish a value
-				stream.onNext("Hello World!");
-				stream.onNext("Goodbye World!");
-				stream.onComplete();
-
-				promise.block();
-		
-		//System.out.println(g.sample.toString()); 
-		
-        return Mono.just(g.getSample().toString());
-    
-    }
-    
-    @RequestMapping("/never")
-    public @ResponseBody  Mono<String> never() {
-    	return Mono.never();
-    }
-   
+	}
 }
